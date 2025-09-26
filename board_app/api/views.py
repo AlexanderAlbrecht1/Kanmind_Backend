c683d8bff2283fc107752743f5b05db352756b54
AlexanderAlbrecht1 <alexander.albrecht1@gmail.com>
2025-09-26 22:07:30 +0200

    start permission

--- board_app/api/views.py
+++ board_app/api/views.py
@@ -1,19 +1,24 @@
 from rest_framework import generics
 from board_app.models import Board, Task
 from .serializer import BoardSerializer, TaskSerializer
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 
 class BoardListCreateView(generics.ListCreateAPIView):
     queryset = Board.objects.all()
     serializer_class = BoardSerializer
+    permission_classes = [IsAuthenticated]
 
 class BoardRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Board.objects.all()
     serializer_class = BoardSerializer
+    permission_classes = []
 
 class TaskListCreateView(generics.ListCreateAPIView):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+    permission_classes = []
 
 class TaskRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+    permission_classes = []
